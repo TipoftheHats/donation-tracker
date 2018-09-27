85172ae460a978ab25a64637052c39b90bd6088e
Justin Kim <justin@justinkim.com>
2018-09-26 21:35:45 -0400

    Create Donors and sundry fixes in import_scraptf_donations

--- management/commands/import_scraptf_donations.py
+++ management/commands/import_scraptf_donations.py
@@ -5,15 +5,18 @@ import traceback
 
 from django.conf import settings
 from django.core.management.base import CommandError
+from django.core.validators import validate_email
 from django.db import IntegrityError, transaction
 from structlog import get_logger
 
-from tracker.models import Donation, DonationBid, BidSuggestion
+from tracker.models import Donor, Donation, Bid, DonationBid, BidSuggestion
 from tracker.scraptf import ScrapTF
 import tracker.commandutil as commandutil
 import tracker.viewutil as viewutil
 
 log = get_logger()
+
+DONOR_ALIAS_MAX_LENGTH = Donor._meta.get_field('alias').max_length
 
 
 class Command(commandutil.TrackerCommand):
@@ -41,13 +44,17 @@ class Command(commandutil.TrackerCommand):
             donations = ScrapTF(key).get_latest_donations(fundraiser, after)
         except Exception as e:
             log.critical('Failed to get latest donations from Scrap.TF.',
-                         exception=repr(e))
+                         exception=repr(e),
+                         fundraiser=fundraiser,
+                         after=after)
             raise CommandError(e)
 
         failed = 0
         for donation in donations:
             try:
                 self.commit_scraptf_donation(donation)
+                log.info('Successfully committed ScrapTF donation.',
+                         donation_id=donation['id'])
             except Exception as e:
                 log.error('Failed to commit ScrapTF donation.',
                           exception=repr(e),
@@ -59,33 +66,53 @@ class Command(commandutil.TrackerCommand):
                  failed=failed,
                  total=len(donations))
 
+    # Can't use latest() here because we fetch ScrapTF donations in bulk, no
+    # guarantees made about order of insertion.
     def get_latest_scraptf_donation(self):
         return Donation.objects.filter(domain__exact='SCRAPTF').order_by('-timereceived').first()
 
     @transaction.atomic
     def commit_scraptf_donation(self, donation):
-        """
-        From a ScrapTF donation, create a Donation. If incentives are
-        present, create all DonationBids and BidSuggestions as well.
-        """
+        '''
+        From a ScrapTF donation, create a Donor and Donation. If
+        incentives are present, create all DonationBids and
+        BidSuggestions as well. We try very hard to ensure everything
+        is valid for insertion. If anything doesn't look right, we
+        fail the entire transaction.
+        '''
 
-        # First, gather all Donation fields
-        domain_id = donation['id']
-        amount = Decimal(donation['cash_value'])
-        time = datetime.fromtimestamp(donation['confirmed_time'], tz=pytz.utc)
-        comment = donation.get('message', '')
-        commentstate = 'PENDING' if len(comment) > 0 else 'ABSENT'
-
-        user = donation.get('user', {})
-        alias = user.get('name')
-        steamid = user.get('steamid')
+        # Create the Donor
+        user = donation['user']
         email = donation.get('email')
+        if not email:
+            raise Exception('Donation is missing an email.')
+        validate_email(email)
+        alias = user.get('name', '')[:DONOR_ALIAS_MAX_LENGTH]
 
-        d = Donation(domain='SCRAPTF',
+        donor = Donor(email=email,
+                      alias=alias,
+                      visibility='ALIAS')
+
+        # The only unique constraint on Donors is paypalemail, which is
+        # not a concern here, so forget about IntegrityError.
+        donor.full_clean()
+        donor.save()
+
+        # Create the Donation
+        domain_id = donation['id']
+        amount = Decimal(str(donation['cash_value']))
+        time = datetime.fromtimestamp(donation['confirmed_time'], tz=pytz.utc)
+        comment = donation.get('message')
+        commentstate = 'PENDING' if comment else 'ABSENT'
+        steamid = user.get('steamid')
+
+        d = Donation(donor=donor,
+                     domain='SCRAPTF',
                      domainId=domain_id,
                      transactionstate='COMPLETED',
                      amount=amount,
                      timereceived=time,
+                     currency='USD',
                      comment=comment,
                      commentstate=commentstate,
                      requestedalias=alias,
@@ -93,32 +120,32 @@ class Command(commandutil.TrackerCommand):
                      steamid=steamid)
 
         try:
+            d.full_clean()
             d.save()
         except IntegrityError:
-            log.debug('Donation already exists, refusing to modify it.',
-                      donation_id=domain_id)
-            return
+            raise Exception('Donation already exists')
 
         # Only proceed if incentives are present
         incentives = donation.get('incentives')
         if not incentives:
             return
-        log.info("Donation has incentives, creating them now.",
+        log.info('Donation has incentives, creating them now.',
                  incentives_total=len(incentives))
 
         # Ensure the incentives don't exceed the total donation amount
         inc_amounts = [Decimal(inc['amount']) for inc in incentives]
         inc_total = sum(inc_amounts)
         if inc_total > amount:
-            raise Exception("sum of incentive amounts exceeds donation amount")
+            raise Exception('sum of incentive amounts exceeds donation amount')
 
         for inc in incentives:
             # First grab the bid to make sure it exists
             bid = Bid.objects.get(pk=inc['incentive'])
 
             dbid = DonationBid(donation=d,
-                              bid=bid,
-                              amount=Decimal(inc['amount']))
+                               bid=bid,
+                               amount=Decimal(inc['amount']))
+            dbid.full_clean()
             dbid.save()
 
             # If the incentive has a custom value, create a BidSuggestion
@@ -126,4 +153,5 @@ class Command(commandutil.TrackerCommand):
             if suggestion:
                 s = BidSuggestion(bid=bid,
                                   name=suggestion)
+                s.full_clean()
                 s.save()
